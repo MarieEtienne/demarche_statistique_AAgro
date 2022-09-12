0ead5756ccb89d8e4ff8f036b360746a6286bdce
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2022-09-12 10:09:26 +0200

    Revert "Change action to avoid the use of build directory"
    This reverts commit bb6132d27bd6ab10c359007d04fbcef399bd85a3.

--- copy_scripts.R
+++ copy_scripts.R
@@ -1,6 +1,6 @@
 filenames <- list.files( pattern = '_script.R',
                         full.names = TRUE)
 
-# for(f_ in filenames){
-#   system(glue::glue( 'cp {f_} build/.'))
-# }
+for(f_ in filenames){
+  system(glue::glue( 'cp {f_} build/.'))
+}
